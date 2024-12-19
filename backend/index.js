3b94cae04c67246a1994512c68ac848e1462f330
preethika-11 <preethikabalak@gmail.com>
2024-12-19 14:02:01 +0530

    env

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,10 @@ app.use(bodyParser.json());
 
 // MySQL Connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Kkpk@1981',
-    database: 'EmployeeDB'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
